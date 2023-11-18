b329c829ed27f63d1c2b37463ea392d996bdd4be
doctorixx <example@example.com>
2023-11-18 13:51:47 +0300

    Auto select when one COM

--- userInput.py
+++ userInput.py
@@ -6,4 +6,11 @@ def getCOM() -> str:
     print("Select COM from list: \n")
     print(*coms, sep="\n")
 
+    if len(coms) == 1:
+        print(f"Auto Selected : {coms[0]}")
+        return coms[0].device
+    elif len(coms) == 0:
+        print("NO COMs")
+        exit()
+
     return input(">>> ")
